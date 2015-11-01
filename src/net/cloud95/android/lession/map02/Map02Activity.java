a7f83a66ac9dcfaf1551d5e0b7876a9bd55940fc
Yves0708 <yvesloveisis@gmail.com>
2015-11-01 15:33:51 +0800

    Map02

---
+++ src/net/cloud95/android/lession/map02/Map02Activity.java
@@ -0,0 +1,46 @@
+package net.cloud95.android.lession.map02;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+
+public class Map02Activity extends Activity {
+
+    private MapView mapView;
+    private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+}
